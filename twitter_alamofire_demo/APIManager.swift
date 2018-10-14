0a3f99b760086ae17169e89d474ff32f9c231afa
Jacob Frick <jacobfrick@Jacobs-MacBook-Air-2.local>
2018-10-13 19:23:34 -0700

    Added user models and fixed certain default glitches

--- twitter_alamofire_demo/APIManager.swift
+++ twitter_alamofire_demo/APIManager.swift
@@ -15,8 +15,8 @@ import KeychainAccess
 class APIManager: SessionManager {
     
     // MARK: TODO: Add App Keys
-    static let consumerKey = "YOUR_KEY_HERE"
-    static let consumerSecret = "YOUR_SECRET_HERE"
+    static let consumerKey = ""
+    static let consumerSecret = ""
 
     static let requestTokenURL = "https://api.twitter.com/oauth/request_token"
     static let authorizeURL = "https://api.twitter.com/oauth/authorize"
@@ -38,7 +38,7 @@ class APIManager: SessionManager {
                 if let error = error {
                     failure(error)
                 } else if let user = user {
-                    print("Welcome \(user.name)")
+                    print("Welcome \(String(describing: user.name))")
                     
                     // MARK: TODO: set User.current, so that it's persisted
                     
---
+++ twitter_alamofire_demo/User.swift
@@ -0,0 +1,24 @@
+//
+//  User.swift
+//  twitter_alamofire_demo
+//
+//  Created by Jacob Frick on 10/13/18.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import Foundation
+class User {
+    
+    // MARK: Properties
+    var name: String?
+    var screenName: String?
+    var profilePicture: URL?
+    init(dictionary: [String: Any]) {
+        name = dictionary["name"] as? String
+        screenName = dictionary["screen_name"] as? String
+        if let profile: String = dictionary["profile_image_url_httpls"] as? String {
+            profilePicture = URL(string: profile)!
+        }
+        // Initialize any other properties
+    }
+}
